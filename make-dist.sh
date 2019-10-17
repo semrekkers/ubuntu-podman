c9915d3c35890dd60d8edd72bc3041e9728c4db0
Sem Rekkers <rekkers.sem@gmail.com>
2019-10-17 19:07:26 +0200

    Add CNI plugins

--- make-dist.sh
+++ make-dist.sh
@@ -9,6 +9,7 @@ if [ "$1" == "--clean" ]; then
         ubuntu-podman-fuse-overlayfs \
         ubuntu-podman-crun \
         ubuntu-podman-conmon \
+        ubuntu-podman-cni-plugins \
         ubuntu-podman-base-go \
         ubuntu-podman-base \
         ubuntu-podman-dep-fuse-overlayfs
@@ -41,12 +42,14 @@ docker build -f Conmon.df -t ubuntu-podman-conmon .
 docker build -f Crun.df -t ubuntu-podman-crun .
 docker build -f Fuse-overlayfs.df -t ubuntu-podman-fuse-overlayfs .
 docker build -f Slirp4netns.df -t ubuntu-podman-slirp4netns .
-
+docker build -f Cni-plugins.df -t ubuntu-podman-cni-plugins .
 docker build -f Podman.df -t ubuntu-podman-podman .
 
 mkdir -p dist/usr/bin
 mkdir -p dist/usr/share/containers
 mkdir -p dist/etc/containers
+mkdir -p dist/etc/cni/net.d
+mkdir -p dist/opt/cni
 
 install-from-image conmon bin/conmon dist/usr/bin/conmon
 install-from-image crun crun dist/usr/bin/crun
@@ -54,10 +57,19 @@ install-from-image fuse-overlayfs fuse-overlayfs dist/usr/bin/fuse-overlayfs
 install-from-image slirp4netns slirp4netns dist/usr/bin/slirp4netns
 install-from-image podman bin/podman dist/usr/bin/podman
 
+echo "Installing CNI plugins..."
+if [ ! -d dist/opt/cni/bin ]; then
+    TMP_CONTAINER=`docker run -d --rm ubuntu-podman-cni-plugins sleep 120`
+    docker cp $TMP_CONTAINER:/cni-plugins.tar.gz deps/
+    docker rm -f $TMP_CONTAINER > /dev/null
+    tar -C dist/opt/cni -xf deps/cni-plugins.tar.gz
+fi
+
 echo "Installing default configuration..."
 cp -n config/registries.conf dist/etc/containers
+cp -n config/policy.json dist/etc/containers
+cp -n config/87-podman-bridge.conflist dist/etc/cni/net.d
 cp -n config/seccomp.json dist/usr/share/containers
-cp -n config/policy.json dist/etc/containers/
 
 echo "Creating distribution..."
 tar -cJf ubuntu-podman.tar.xz dist
